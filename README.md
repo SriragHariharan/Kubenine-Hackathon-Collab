659e468a401c3eaecce019906782b38fe57f0da6
Srirag Hariharan <sriraghariharan108@gmail.com>
2025-10-18 16:57:19 +0530

    Created a basic react app and  configured tailwind css.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kubenine Hackathon/ Hirethon 2025
+
+## Task: Slack like react application with rocket api integration
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-500 text-8xl'>App</div>
+  )
+}
+
+export default App
